17ce018e1bee54e068f83a84f95657e97b44d9b0
Akil Lutes <lutes.akil@gmail.com>
2023-03-25 22:24:40 -0400

    added fetchData component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { useState, useEffect } from 'react'
+
 import Title from './components/Title'
 
 const App = () => {
+  const [quiz, setQuiz] = useState(false)
+
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/quiz')
+      const json = await response.json()
+      console.log(json)
+      // Overriding the value of false in useState
+      setQuiz(json)
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
+      <Title/>
       
     </div>
   )
